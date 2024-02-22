5b04fdbd9fd8cff54a020ce7c15b8519caf02407
ayalaillouz <a0534195130@gmail.com>
2024-02-22 19:37:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using Generix;
+
+Console.WriteLine("Hello, World!");
+static void PrintTheObj<T>(T obj)
+{
+    Console.WriteLine(obj);
+}
+PrintTheObj<int>(1);
+PrintTheObj<string>("aysls");
+
+SwapList<int> swapList = new SwapList<int>();
+swapList.Add(1);
+swapList.Add(2);
+swapList.Add(3);
+swapList.Add(4);
+swapList.Add(5);
+Console.WriteLine("x:" + swapList[0] + "y:" + swapList[1]);
+swapList.swaper(0, 1);
+Console.WriteLine("x:" + swapList[0] + "y:" + swapList[1]);
---
+++ SwapList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generix
+{
+    public class SwapList<T> : List<T>
+    {
+        public SwapList() { }
+
+        public void swaper(int index1, int index2)
+        {
+            T temp = this[index1];
+            this[index1] = this[index2];
+            this[index2] = temp;
+        }
+    }
+}
